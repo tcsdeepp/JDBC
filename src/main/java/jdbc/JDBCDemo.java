afd9e4301ebdc27f6b44e14efd28e587b892689b
deepp <tcsdeepp@gmail.com>
2021-08-16 09:41:06 +0000

    SL4j with log4j

--- src/main/java/jdbc/JDBCDemo.java
+++ src/main/java/jdbc/JDBCDemo.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
+import org.slf4j.Logger; //important
 import org.slf4j.LoggerFactory;
 
 public class JDBCDemo {
